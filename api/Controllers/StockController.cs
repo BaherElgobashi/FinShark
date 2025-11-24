c5df5a8966d2d7e07a942c106e85e67935ed618e
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-11-24 11:59:37 +0200

    Add StockController then Make some Endpoints in it.

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+
+    public class StockController : ControllerBase
+    {
+    private readonly ApplicationDbContext _context;
+    public StockController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+
+    public IActionResult GetAll()
+    {
+        var stocks = _context.Stocks.ToList();
+        return Ok(stocks);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var stock = _context.Stocks.Find(id);
+        if(stock is null)
+            return NotFound();
+        return Ok(stock);
+            
+    }
+
+
+
+
+    }
+    
+    
+}
