4496c61ccc31c2211e90702da7a647a8f4f8f034
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-11-25 15:42:10 +0200

    Make The CommentController then Make it use CommentRepository.

---
+++ api/Controllers/CommentController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Logging;
+
+namespace api.Controllers
+{
+
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentController : Controller
+    {
+        private readonly ICommentRepository _commentRepository;
+        public CommentController(ICommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetAll()
+        {
+            var Comments = await _commentRepository.GetAllAsync();
+
+            return Ok(Comments);
+        }
+        
+    }
+}
