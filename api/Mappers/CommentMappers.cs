1727d83e441307424cd9fdabc8280dbb765e379e
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-11-26 13:34:32 +0200

    Make CommentMappers tha helps in manual Mapping.

---
+++ api/Mappers/CommentMappers.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Comment;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class CommentMappers
+    {
+        public static CommentDto toCommentDto(this Comment CommentModel)
+        {
+            return new CommentDto
+            {
+              Id = CommentModel.Id,
+              Title = CommentModel.Title,
+              Content = CommentModel.Content,
+              CreatedOn = CommentModel.CreatedOn
+            };
+        }
+        
+    }
+}
