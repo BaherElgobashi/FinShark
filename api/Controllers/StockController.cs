86b47599c215eabb3013b4b6250664d9801fd3ab
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-11-29 14:03:39 +0200

    Check the ModelState in all StockController Endpoints to check up the Data Validation.

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -30,6 +30,8 @@ namespace api.Controllers
 
     public async Task<IActionResult> GetAll()
     {
+        if(!ModelState.IsValid)
+                return BadRequest(ModelState);
         var stocks = await _stockRepo.GetAllAsync();
         var stockDto = stocks.Select(s => s.ToStockDto());
         return Ok(stockDto);
@@ -38,6 +40,8 @@ namespace api.Controllers
     [HttpGet("{id:int}")]
     public async Task<IActionResult> GetById([FromRoute]int id)
     {
+        if(!ModelState.IsValid)
+                return BadRequest(ModelState);
         var stock = await _stockRepo.GetByIdAsync(id);
         if(stock is null)
             return NotFound();
@@ -48,6 +52,8 @@ namespace api.Controllers
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] CreatedStockRequestDto stockDto)
     {
+        if(!ModelState.IsValid)
+                return BadRequest(ModelState);
         var stockModel =  stockDto.ToStockFromCreateDto();
         // await _context.Stocks.AddAsync(stockModel);
         // await _context.SaveChangesAsync();
@@ -60,6 +66,8 @@ namespace api.Controllers
     [Route("{id:int}")]
     public async Task<IActionResult> Update([FromRoute] int id , [FromBody] UpdateStockRequestDto updateDto)
     {
+        if(!ModelState.IsValid)
+                return BadRequest(ModelState);
         var stockModel = await _stockRepo.UpdateAsync(id,updateDto);
         if(stockModel is null)
             return NotFound();
@@ -81,6 +89,8 @@ namespace api.Controllers
     [Route("{id:int}")]
     public async Task<IActionResult> Delete([FromRoute] int id)
     {
+        if(!ModelState.IsValid)
+                return BadRequest(ModelState);
         var stockModel = await _stockRepo.DeleteAsync(id);
         if(stockModel is null)
             return NotFound();
