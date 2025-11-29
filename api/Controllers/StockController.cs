120539be97a19c94f5b0efed32d1800468b6d445
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-11-29 13:39:22 +0200

    Add Constraints to StockController Verbs.

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -35,7 +35,7 @@ namespace api.Controllers
         return Ok(stockDto);
     }
 
-    [HttpGet("{id}")]
+    [HttpGet("{id:int}")]
     public async Task<IActionResult> GetById([FromRoute]int id)
     {
         var stock = await _stockRepo.GetByIdAsync(id);
@@ -57,7 +57,7 @@ namespace api.Controllers
     }
 
     [HttpPut]
-    [Route("{id}")]
+    [Route("{id:int}")]
     public async Task<IActionResult> Update([FromRoute] int id , [FromBody] UpdateStockRequestDto updateDto)
     {
         var stockModel = await _stockRepo.UpdateAsync(id,updateDto);
@@ -78,7 +78,7 @@ namespace api.Controllers
     }
 
     [HttpDelete]
-    [Route("{id}")]
+    [Route("{id:int}")]
     public async Task<IActionResult> Delete([FromRoute] int id)
     {
         var stockModel = await _stockRepo.DeleteAsync(id);
