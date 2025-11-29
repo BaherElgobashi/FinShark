7e30a698462dafcfe7ce9296c604a51454d41eec
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-11-29 13:48:07 +0200

    Add Constraints by using Data Annotation in CreatedCommentRequestDto.

--- api/Dtos/Comment/CreatedCommentDto.cs
+++ api/Dtos/Comment/CreatedCommentDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,13 @@ namespace api.Dtos.Comment
 {
     public class CreatedCommentDto
     {
-        
+        [Required]
+        [MinLength(5,ErrorMessage ="Title must be 5 Characters.")]
+        [MaxLength(280,ErrorMessage ="Title can't be over 280 Characters.")]
         public string Title { get; set; } = string.Empty;
+        [Required]
+        [MinLength(5,ErrorMessage ="Content must be 5 Characters.")]
+        [MaxLength(280,ErrorMessage ="Content can't be over 280 Characters.")]
         public string Content { get; set; } = string.Empty;
         
         
--- api/Dtos/Comment/UpdateCommentRequestDto.cs
+++ api/Dtos/Comment/UpdateCommentRequestDto.cs
@@ -13,8 +13,8 @@ namespace api.Dtos.Comment
         [MaxLength(280,ErrorMessage ="Title can't be over 280 Characters.")]
         public string Title { get; set; } = string.Empty;
         [Required]
-        [MinLength(5,ErrorMessage ="Title must be 5 Characters.")]
-        [MaxLength(280,ErrorMessage ="Title can't be over 280 Characters.")]
+        [MinLength(5,ErrorMessage ="Content must be 5 Characters.")]
+        [MaxLength(280,ErrorMessage ="Content can't be over 280 Characters.")]
         public string Content { get; set; } = string.Empty;
     }
 }
