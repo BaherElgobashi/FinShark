13b7b4c392e213f242120aee898713efb4ed3577
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-04 14:42:12 +0200

    Add Register Endpoint to the AccountController.

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Dtos.Account;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -20,5 +21,43 @@ namespace api.Controllers
        {
         _userManager = userManager;
        }
+
+       [HttpPost("register")]
+       public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            try
+            {
+                if(!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var appUser = new AppUser
+                {
+                    UserName = registerDto.UserName,
+                    Email = registerDto.Email,
+                };
+
+                var CreatedUser = await _userManager.CreateAsync(appUser,registerDto.Password);
+                if (CreatedUser.Succeeded)
+                {
+                    var roleResult = await _userManager.AddToRoleAsync(appUser,"User");
+                    if (roleResult.Succeeded)
+                    {
+                        return Ok("User is Created.");
+                    }
+                    else
+                    {
+                        return StatusCode(500,roleResult.Errors);
+                    }
+                }
+                else
+                {
+                    return StatusCode(500,CreatedUser.Errors);
+                }
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500,e)
+            }
+        }
     }
 }
