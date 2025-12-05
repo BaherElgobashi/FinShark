31a4727f43f7f8381ea539158103cb44056b7929
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-12-05 22:56:24 +0200

    Add The Portfolio DbSet to the ApplicationDbContext , then Make the many-to-many Relationship using the FluentApi.

--- api/Data/ApplicationDbContext.cs
+++ api/Data/ApplicationDbContext.cs
@@ -20,9 +20,27 @@ namespace api.Data
         public DbSet<Stock>Stocks {get;set;} 
         public DbSet<Comment> Comments { get; set; }
 
+        public DbSet<Portfolio> Portfolios {get;set;}
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+
+            // Make Composite key from AppUserId and StockId which is declared in the Portfolio Module.
+            builder.Entity<Portfolio>(x=>x.HasKey(k=>new{k.AppUserId,k.StockId}));
+
+            // Make Relationship with AppUser.
+            builder.Entity<Portfolio>()
+            .HasOne(a=>a.AppUser)
+            .WithMany(a => a.Portfolios)
+            .HasForeignKey(a=> a.AppUserId);
+
+            // Make Relationship with Stock.
+            builder.Entity<Portfolio>()
+            .HasOne(s=>s.Stock)
+            .WithMany(s=>s.Portfolios)
+            .HasForeignKey(s=>s.StockId);
             
 
             List<IdentityRole> roles = new List<IdentityRole>
